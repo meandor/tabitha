517812736eb6cd3e503b58ae2a0346750d7a75a3
Daniel Schruhl <danielschruhl@gmail.com>
2019-08-12 19:40:02 +0200

    fix exports

--- src/logger.ts
+++ src/logger.ts
@@ -1,11 +1,9 @@
 import * as winston from "winston";
 
-const logger = winston.createLogger({
+export const logger = winston.createLogger({
   format: winston.format.json(),
   level: "info",
   transports: [
     new winston.transports.Console({ format: winston.format.simple() })
   ]
 });
-
-export default logger;
